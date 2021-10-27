c65fb72e0ec33dad9795939595852ae4dc79c005
husnain461 <71745783+husnain461@users.noreply.github.com>
2021-10-26 23:35:21 -0400

    Add files via upload

---
+++ db.js
@@ -0,0 +1,6 @@
+module.exports = {
+    //local MongoDB deployment ->
+    //"URI": "mongodb://localhost/book229"
+    //"URI": "mongodb://localhost/books229"
+    "URI": "mongodb+srv://Hao:Xx9MaIlDlaaqtsHB@mongodbserver.ygw6v.mongodb.net/book229?retryWrites=true&w=majority"
+  };
