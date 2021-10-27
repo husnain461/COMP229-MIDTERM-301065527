3ef78d048c00e8abde2fa10e1a1d39e165769bbe
husnain461 <71745783+husnain461@users.noreply.github.com>
2021-10-26 23:38:38 -0400

    Add files via upload

---
+++ books.js
@@ -0,0 +1,16 @@
+//Husnain ali, 301065527
+let mongoose = require('mongoose');
+
+// create a model class
+let Book = mongoose.Schema({
+    title: String,
+    description: String,
+    price: Number,
+    author: String,
+    genre: String
+},
+{
+  collection: "books"
+});
+
+module.exports = mongoose.model('Book', Book);
